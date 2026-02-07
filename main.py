ef3a3cd48e7b03e4438e39515de024cb74a88437
Luis A. <owlreaper3327@gmail.com>
2026-02-06 22:21:19 -0500

    Adding nested conversion and default config
    I usually save my pictures to convert in a very specific folder. So, I added some neat lines to allow nested convert all the folders into that one. That's it.

--- main.py
+++ main.py
@@ -4,12 +4,25 @@ import os
 import img2pdf
 from PIL import Image
 
+#Here we'll declare constants
+
+
+objdir = "" #variable for image's dir path
+final_name = "" #defines the final name of the resultant pdf
+imgs = [] #Stores the images in dir
+queue = [] #queue of paths
+progress = 0 #progress
+com = ""
+COMMANDS = {}
 
 
 #Here we'll define functions
 
 def convert_webp(img, name):
     """Due a significant compatibility problem with .webp images, I added this funny method"""
+
+    if not os.path.exists("temp/"):
+        os.mkdir("temp")
 
     image = Image.open(img).convert("RGB")
     ruta_relativa = os.path.join("temp/", name.split(".")[0] + ".jpg")
@@ -18,7 +31,7 @@ def convert_webp(img, name):
     return ruta_abs
 
 def extract_imgs(path_to):
-    """This function will perform a search in the dir you pass in the params"""
+    """This function will perform a search in the dir you pass in the params and retrun the images"""
     
     imgs = []
     
@@ -61,51 +74,171 @@ def convert_pdf(path_to, final_name, imgs):
         os.remove(path)
 
 
+def extract_queue(route):
+    """Given the default path, this will extract all the folders to convert and put them in the queue"""
+    
+    path = ""
+
+    try:
+        with open("save.txt", "r") as sav:
+            path = sav.readline()
+
+    except Exception:
+        print("Unreadable sav file or there's no default path setted")
+        print("Aborting operation...")
+        return
+    else:
+        for fi_name in os.listdir(path):
+            path_2 = os.path.join(path, fi_name)
+
+            if not os.path.isdir(path_2):
+                continue
+
+            queue.append((fi_name, path_2))
+    
+    return
+
+
+
+def complete_conversion():
+    """This script will execute the conversion for an already done queue"""
+    progress = 0
+    for name, dir in queue:
+        imgs = extract_imgs(dir)
+        convert_pdf(dir, name, imgs)
+        print("\a")
+        progress += 1
+        print(f"Actual progress {progress}/{len(queue)}")
+        print(f"{(progress/len(queue)) * 100:.2f}% completed")
+    
+    print("\a")
+    print("\a")
+    print("FINISHED!")
+    return
+
+
+def main_path():
+
+    while(True):
+        try:
+            objdir = input("Introduce the complete path to your pictures folder: ")
+
+            if not (os.path.exists(objdir)):
+                raise OSError("Path included doesn't exist")
+
+            if not (os.path.isdir(objdir)):
+                raise OSError("Wrong input, must be a folder")
+            
+            final_name = input("Introduce the name to your resultant file: ")
+            
+            if final_name == "?def" or final_name == "":
+                final_name = objdir.split("/")[-1]
+
+            queue.append((final_name, objdir))
+
+        except OSError:
+            print("Path you included is not valid")
+            continue
+        else:
+            val = input("Do you want to add more files? (s/n)\n")
+
+            if val.lower() == "s":
+                continue
+            else:
+                break
+    
+    complete_conversion()
+    return
+
+
+
+
+def take_default_path():
+    """This is for establishing the default path for conversion"""
+    """With this option, app will save the path in a txt (later i'll do a propper savefile)"""
+    """Later, will use this path to convert all the directories there into pdf files"""
+
+    try:
+        with open("save.txt", "r") as sav:
+            objdir = sav.readline()
+
+    except OSError:
+        print("Save file not found. Create one? (y/n)")
+        c = input()
+
+        if c.lower == "y":
+            set_default_path()
+        else:
+            return
+    else:
+        extract_queue(objdir)
+        complete_conversion()
+    
+    return
+
+
+def set_default_path():
+    """This will set the default path to recursive conversion"""
+
+    path = input("Introduce the full path to your conversion site: ")
+
+    while(True):
+        if not os.path.exists(path):
+            print("The introduced path it's no valid")
+            continue
+            
+        if not os.path.isdir(path):
+            print("The introduced path must be a directory")
+            continue
+
+        with open("save.txt", "w") as sav:
+            sav.write(path)
+            break
+    
+    return
+    
+def display_guide():
+    """This function will show available commands to execute"""
+
+    print("Welcome to the Owl's simple pdf converter\nWhat do you want today?")
+
+    print("List of commands:\n")
+    print("?def to go on and convert default route")
+    print("?cdef to change default route")
+    print("?exit to end")
+    print("")
+
+    print("Or simply press enter to convert one to one")
+
 
 #Here we'll define the main logic of the program
 
-objdir = "" #variable for image's dir path
-final_name = "" #defines the final name of the resultant pdf
-imgs = [] #Stores the images in dir
-queue = [] #queue of paths
-progress = 0 #progress
+COMMANDS = {"" : main_path, "?def":take_default_path, "?cdef":set_default_path}
 
 while(True):
-    try:
-        objdir = input("Introduce the complete path to your pictures folder: ")
 
-        if not (os.path.exists(objdir)):
-            raise OSError("Path included doesn't exist")
+    objdir = "" #variable for image's dir path
+    final_name = "" #defines the final name of the resultant pdf
+    imgs = [] #Stores the images in dir
+    queue = [] #queue of paths
+    progress = 0 #progress
+    com = ""
 
-        if not (os.path.isdir(objdir)):
-            raise OSError("Wrong input, must be a folder")
-        
-        final_name = input("Introduce the name to your resultant file: ")
+    display_guide()
+    com = input("Introduce your command: ")
 
-        queue.append((final_name, objdir))
-
-    except OSError:
-        print("Path you included is not valid")
-        continue
+    if com == "?cdef":
+        set_default_path()
+    elif com == "":
+        main_path()
+    elif com == "?def":
+        take_default_path()
+    elif com == "?exit":
+        break
     else:
-        val = input("Do you want to add more files? (s/n)\n")
+        print("The specified word isn't a valid command")
+        continue
 
-        if val.lower() == "s":
-            continue
-        else:
-            break
-
-for name, dir in queue:
-    imgs = extract_imgs(dir)
-    convert_pdf(dir, name, imgs)
-    print("\a")
-    progress += 1
-    print(f"Actual progress {progress}/{len(queue)}")
-    print(f"{(progress/len(queue)) * 100:.2f}% completed")
-
-print("\a")
-print("\a")
-print("FINISHED!")
 
 
 #Here we'll clean up
